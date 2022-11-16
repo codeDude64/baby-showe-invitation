6cdafb519524b6c4b59a33294d7a7422a22cf3de
codeDude <codeDude64@proton.me>
2022-11-15 22:06:05 -0600

    make info desc

--- src/components/InvitationContainer/Information/Infomation.tsx
+++ src/components/InvitationContainer/Information/Infomation.tsx
@@ -1,4 +1,6 @@
-import { Card, CardContent, CardMedia, Link, Typography } from '@mui/material';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import MapIcon from '@mui/icons-material/Map';
+import { Card, CardContent, Link, Typography } from '@mui/material';
 
 import useStyles from './useStyles';
 
@@ -8,9 +10,18 @@ const Information = () => {
     <Card className={classes.container}>
       <CardContent>
         <Typography pb={4} pt={2} className={classes.title}>
-          Dirección
+          Acerca de
         </Typography>
         <Typography className={classes.desc}>
+          Nos complace extenderte la invitacion para que nos acompanies el dia{' '}
+        </Typography>
+        <Typography className={classes.desc}>
+          <CalendarMonthIcon />
+          Domingo 27 de Noviembre a partir de las 2:00pm. En el link adjunto encontraras
+          los detalles con la dirección.
+        </Typography>
+        <Typography className={classes.desc}>
+          <MapIcon />
           San Isidro 18, San Ramón, Zamora Michoacán.
         </Typography>
         <Link className={classes.desc} href="https://maps.app.goo.gl/qX8P6Jgo8PDztHeP6">
